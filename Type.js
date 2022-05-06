3982761ab0212fe0e1eb0643094bc6a3e58e77a3
Darkshadows61 <bisomt@gmail.com>
2022-05-06 00:57:22 -0400

    5.6.22 update1
    -maybe figured out how to use option value to access a obj

--- Type.js
+++ Type.js
@@ -2,69 +2,76 @@
 
 //Page Load
 document.querySelector('#go').addEventListener('click', brock);
-document.querySelector('#go').addEventListener('click', effectiveCheck);
-document.getElementById('results').hidden = true;
-
+//document.querySelector('#go').addEventListener('click', effectiveCheck);
+//document.getElementById('results').hidden = true;
 
 //PokeType Constructor
 class PokeType{
-    constructor(name, type, color, strong, weak, resist, immune) {
+    constructor(name, color, strong, weak, immune) {
     this.name = name
-    this.type = type
     this.color = color
     this.strongAgainst = strong
     this.weakTo = weak
-    this.resistantTo = resist
     this.immuneTo = immune
   }
 }
 
 //PokeType Objects
-const opener = new PokeType('Select', 'none', 'clear', ['none'], ['none'], ['none'], ['none'])
-const normal = new PokeType('Normal', 'normal', "grey", ['none'], ['fighting'], ['none'], ['ghost'])
-const fire = new PokeType('Fire', 'fire', 'red', ['grass', 'ice', 'bug', 'steel'], ['water', 'ground', 'rock'], ['fire'], ['none'])
-const water = new PokeType('Water', 'water',  'blue', ['fire', 'ground', 'rock'],['grass', 'electric'],['none'],['none'])
-const electric = new PokeType('Electric', 'electric', 'yellow', [''],[''],[''],[''])
+const opener = new PokeType('Select', 'clear', ['none'], ['none'], ['none'])
+const normal = new PokeType('Normal', 'grey', ['nothing'], ['fighting'], ['ghost'])
+const fire = new PokeType('Fire', 'red', ['grass', 'ice', 'bug', 'steel'], ['water', 'ground', 'rock'], ['none'])
+const water = new PokeType('Water',  'blue', ['fire', 'ground', 'rock'],['grass', 'electric'], ['none'])
+const electric = new PokeType('Electric', 'yellow', ['water', 'flying'], [''], [''])
 
 //List Container
 const typeName = [opener, normal, fire, water, electric]
 
 //Attack List
-typeName.forEach(function(PokeType){
-    var option = document.createElement('option');
-    option.value = PokeType.strongAgainst
-    option.innerHTML = PokeType.name;
+typeName.forEach(function(li){
+    let option = document.createElement('option');
+    option.value = li.name;
+    option.innerHTML = li.name;
     selectorAttack.appendChild(option)
 })
 
 //Defense1 List
 typeName.forEach(function(PokeType){
-    var option = document.createElement('option');
-    option.value = PokeType.weakTo
+    let option = document.createElement('option');
+    option.value = PokeType.type
     option.innerHTML = PokeType.name;
     selectorDefense1.appendChild(option)
 })
 
 //Defense2 list
 typeName.forEach(function(PokeType){
-    var option = document.createElement('option');
-    option.value = PokeType.weakTo;
+    let option = document.createElement('option');
+    option.value = PokeType.type
     option.innerHTML = PokeType.name;
     selectorDefense2.appendChild(option)
 })
 
 //Brock, what is this type Effective Against?
-function brock(type) {
-    return `${type.name} is strong against ${type.strongAgainst} and is weak against ${type.weakTo}`
+function brock() {
+    let strong;
+    let weak;
+    let immune;
+    let type = document.querySelector('#selectorAttack').value;
+    if (type === 'Normal') {
+       strong = normal.strongAgainst;
+       weak = normal.weakTo;
+       immune = normal.immuneTo;
+       console.log(strong, weak, immune)
+    }
+    document.querySelector('#test').innerHTML = `${type} is `
+    //console.log(`${type.name} is strong against ${type.strongAgainst} and is weak to ${type.weakTo}`)
 }
 
 //Brock, how much damage will this type do against 1 or 2 defense types?
 //this doens't entirely work yet
-function effectiveCheck(){
-    let atk = document.getElementById('selectorAttack').value
-    let def1 = document.getElementById('selectorDefense1').value
-    let def2 = document.getElementById('selectorDefense2').value
-    console.log(atk)
+function effectiveCheck(atk, def1, def2){
+    //console.log(atk.strongAgainst);
+    //console.log(def1.weakTo);
+    //console.log(def2.weakTo);
 }
 
 //set? for sorting an array
