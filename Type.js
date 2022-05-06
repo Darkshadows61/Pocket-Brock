4f53e7e25a2f88da6cff756560931d82c25e34ba
noaher <19noahhughes@gmail.com>
2022-05-06 01:32:24 -0600

    Update Type.js

--- Type.js
+++ Type.js
@@ -19,9 +19,25 @@ class PokeType{
 //PokeType Objects
 const opener = new PokeType('Select', 'clear', ['none'], ['none'], ['none'])
 const normal = new PokeType('Normal', 'grey', ['nothing'], ['fighting'], ['ghost'])
-const fire = new PokeType('Fire', 'red', ['grass', 'ice', 'bug', 'steel'], ['water', 'ground', 'rock'], ['none'])
+const fire = new PokeType('Fire', 'orange', ['grass', 'ice', 'bug', 'steel'], ['water', 'ground', 'rock'], ['none'])
 const water = new PokeType('Water',  'blue', ['fire', 'ground', 'rock'],['grass', 'electric'], ['none'])
 const electric = new PokeType('Electric', 'yellow', ['water', 'flying'], [''], [''])
+const rock = new PokeType('Rock', 'Tan', ['fire', 'ice', 'flying', 'bug'], ['grass', 'water', 'ground', 'fighting', 'steel'], [])
+const poison = new PokeType('Poison', 'Purple', ['Grass', 'Fighting'], ['Psychic', 'Ground'], [])
+const psychic = new PokeType('Psychic', 'pink', ['poison', 'fighting'], ['bug', 'ghost', 'dark'], [])
+const ice = new PokeType('Ice', 'Cyan', ['Dragon', 'Flying', 'Grass'], ['Fire', 'Rock', 'Fighting', 'Steel'], [])
+const ground = new PokeType('Ground', 'Brown', ['Fire', , 'Rock', 'Poison', 'Steel', 'Electric'], ['Grass', 'Water', 'Ice'], ['Electric'])
+//Electric added to both strong against and immune array
+const flying = new PokeType('Flying', 'Light Blue', ['Grass', 'Fighting', 'Bug'], ['Electric', 'Rock', 'Ice'], [])
+const fighting = new PokeType('Fighting', 'Red', ['Rock', 'Ice', 'Dark', 'Steel'], ['Psychic', 'Flying', 'Fairy'], [] )
+const  bug = new PokeType('Bug', 'Green', ['Grass', 'Psychic', 'Dark'], ['Fire', 'Flying', 'Rock'], [])
+const ghost = new PokeType('Ghost', 'Lavender', ['Psychic', 'Ghost'], ['Ghost', 'Dark'], [])
+const dragon = new PokeType('Dragon', 'Blue', ['Dragon'], ['Ice', 'Dragon', 'Fairy'], [])
+const dark = new PokeType('Dark', 'Black', ['Psychic', 'Ghost'], ['Fighting', 'Bug', 'Fairy'], ['Psychic'])
+//got called to an intubation, couldn't remember if dark is completely immune to psychic
+const steel = new PokeType('Steel', 'Gray', ['Rock', 'Ice', 'Fairy'], ['Fire', 'Ground', 'Fighting'], [])
+const fairy = new PokeType('Fairy', 'Pink', ['Fighting', 'Dragon', 'Dark'], ['Poison', 'Dark'], [])
+
 
 //List Container
 const typeName = [opener, normal, fire, water, electric]
