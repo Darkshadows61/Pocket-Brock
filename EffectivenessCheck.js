da66d86840a35b4ebae13ba96f765d99e699b807
Thomas Bisom <bisomt@gmail.com>
2022-05-20 16:53:38 -0400

    5.20.22 Update 1
    -Added some style
    -worked on adding in 1x effect check

--- EffectivenessCheck.js
+++ EffectivenessCheck.js
@@ -2,6 +2,7 @@
 
 //Page Load
 document.querySelector('#go').addEventListener('click', effectivenessCheck);
+const Types = [' Normal', ' Fire', ' Water', ' Grass', ' Electric', ' Rock', ' Poison', ' Psychic', ' Ice', ' Ground', ' Flying', ' Fighting', ' Bug', ' Ghost', ' Dragon', ' Dark', ' Steel', ' Fairy']
 
 //PokeType Constructor
 class PokeType{
@@ -16,26 +17,25 @@ class PokeType{
 }
 
 //PokeType Objects
-//got called to an intubation, couldn't remember if dark is completely immune to psychic - Perserving for comedic value
 const opener = new PokeType('Select', 'clear', ['None'], ['None'], ['None'], ['None'])
-const normal = new PokeType('Normal', 'grey', ['Nothing'], ['Fighting'], ['None'], ['Ghost'])
-const fire = new PokeType('Fire', 'orange', ['Grass', 'Ice', 'Bug', 'Steel'], ['Water', 'Ground', 'Rock'], ['Fire', 'Grass', 'Ice', 'Bug', 'Steel', 'Fairy'], ['None'])
-const water = new PokeType('Water',  'blue', ['Fire', 'Ground', 'Rock'], ['Grass', 'Electric'], ['Fire', 'Water', 'Ice', 'Steel'], ['None'])
-const grass = new PokeType('Grass', 'green', ['Water', 'Ground', 'Rock'],['Fire', 'Ice', 'Poison', 'Flying', 'Bug'], ['Water', 'Electric', 'Grass', 'Ground'], ['None'])
-const electric = new PokeType('Electric', 'yellow', ['Water', 'Flying'], ['Ground'], ['Electric', 'Flying', 'Steel'], ['None'])
-const rock = new PokeType('Rock', 'Tan', ['Fire', 'Ice', 'Flying', 'Bug'], ['Grass', 'Water', 'Ground', 'Fighting', 'Steel'], ['Normal', 'Fire', 'Poison', 'Flying'], ['None'])
-const poison = new PokeType('Poison', 'Purple', ['Grass', 'Fighting'], ['Psychic', 'Ground'], ['Grass', 'Fighting', 'Poison', 'Bug', 'Fairy'], ['None'])
-const psychic = new PokeType('Psychic', 'pink', ['Poison', 'Fighting'], ['Bug', 'Ghost', 'Dark'], ['Fighting', 'Psychic'], ['None'])
-const ice = new PokeType('Ice', 'Cyan', ['Dragon', 'Flying', 'Grass'], ['Fire', 'Rock', 'Fighting', 'Steel'], ['Ice'], ['None'])
-const ground = new PokeType('Ground', 'Brown', ['Fire', , 'Rock', 'Poison', 'Steel', 'Electric'], ['Grass', 'Water', 'Ice'], ['Poison', 'Rock'], ['Electric'])
-const flying = new PokeType('Flying', 'Light Blue', ['Grass', 'Fighting', 'Bug'], ['Electric', 'Rock', 'Ice'], ['Grass', 'Fighting', 'Bug'], ['Ground'])
-const fighting = new PokeType('Fighting', 'Red', ['Rock', 'Ice', 'Dark', 'Steel'], ['Psychic', 'Flying', 'Fairy'], ['Bug', 'Rock', 'Dark'], ['None'] )
-const bug = new PokeType('Bug', 'Green', ['Grass', 'Psychic', 'Dark'], ['Fire', 'Flying', 'Rock'], ['Grass', 'Fighting', 'Ground'], ['None'])
-const ghost = new PokeType('Ghost', 'Lavender', ['Psychic', 'Ghost'], ['Ghost', 'Dark'], ['Poison', 'Bug'], ['Normal', 'Fighting'])
-const dragon = new PokeType('Dragon', 'Blue', ['Dragon'], ['Ice', 'Dragon', 'Fairy'], ['Fire', 'Water', 'Electric', 'Grass'], ['None'])
-const dark = new PokeType('Dark', 'Black', ['Psychic', 'Ghost'], ['Fighting', 'Bug', 'Fairy'], ['Ghost', 'Dark'], ['Psychic'])
-const steel = new PokeType('Steel', 'Gray', ['Rock', 'Ice', 'Fairy'], ['Fire', 'Ground', 'Fighting'], ['Normal', 'Grass', 'Ice', 'Flying', 'Psychic', 'Bug', 'Rock', 'Dragon', 'Steel', 'Fairy'], ['Poison'])
-const fairy = new PokeType('Fairy', 'Pink', ['Fighting', 'Dragon', 'Dark'], ['Poison', 'Dark'], ['Fighting', 'Bug', 'Dark'], ['Dragon'])
+const normal = new PokeType(' Normal', 'grey', ['Nothing'], [' Fighting'], [' None'], [' Ghost'])
+const fire = new PokeType(' Fire', 'orange', [' Grass', ' Ice', ' Bug', ' Steel'], [' Water', ' Ground', ' Rock'], [' Fire', ' Grass', ' Ice', ' Bug', ' Steel', ' Fairy'], [' None'])
+const water = new PokeType(' Water',  'blue', [' Fire', ' Ground', ' Rock'], [' Grass', ' Electric'], [' Fire', ' Water', ' Ice', ' Steel'], [' None'])
+const grass = new PokeType(' Grass', 'green', [' Water', ' Ground', ' Rock'],[' Fire', ' Ice', ' Poison', ' Flying', ' Bug'], [' Water', ' Electric', ' Grass', ' Ground'], [' None'])
+const electric = new PokeType(' Electric', 'yellow', [' Water', ' Flying'], [' Ground'], [' Electric', ' Flying', ' Steel'], [' None'])
+const rock = new PokeType(' Rock', ' Tan', [' Fire', ' Ice', ' Flying', ' Bug'], [' Grass', ' Water', ' Ground', ' Fighting', ' Steel'], [' Normal', ' Fire', ' Poison', ' Flying'], [' None'])
+const poison = new PokeType(' Poison', 'Purple', [' Grass', ' Fighting'], [' Psychic', ' Ground'], [' Grass', ' Fighting', ' Poison', ' Bug', ' Fairy'], [' None'])
+const psychic = new PokeType(' Psychic', 'pink', [' Poison', ' Fighting'], [' Bug', ' Ghost', ' Dark'], [' Fighting', ' Psychic'], [' None'])
+const ice = new PokeType(' Ice', ' Cyan', [' Dragon', ' Flying', ' Grass'], [' Fire', ' Rock', ' Fighting', ' Steel'], [' Ice'], [' None'])
+const ground = new PokeType(' Ground', 'Brown', [' Fire',' Rock', ' Poison', ' Steel', ' Electric'], [' Grass', ' Water', ' Ice'], [' Poison', ' Rock'], [' Electric'])
+const flying = new PokeType(' Flying', ' Light Blue', [' Grass', ' Fighting', ' Bug'], [' Electric', ' Rock', ' Ice'], [' Grass', ' Fighting', ' Bug'], [' Ground'])
+const fighting = new PokeType(' Fighting', 'Red', [' Rock', ' Ice', ' Dark', ' Steel'], [' Psychic', ' Flying', ' Fairy'], [' Bug', ' Rock', ' Dark'], [' None'] )
+const bug = new PokeType(' Bug', 'Green', [' Grass', ' Psychic', ' Dark'], [' Fire', ' Flying', ' Rock'], [' Grass', ' Fighting', ' Ground'], [' None'])
+const ghost = new PokeType(' Ghost', 'Lavender', [' Psychic', ' Ghost'], [' Ghost', ' Dark'], [' Poison', ' Bug'], [' Normal', ' Fighting'])
+const dragon = new PokeType(' Dragon', 'Blue', [' Dragon'], [' Ice', ' Dragon', ' Fairy'], [' Fire', ' Water', ' Electric', ' Grass'], [' None'])
+const dark = new PokeType(' Dark', 'Black', [' Psychic', ' Ghost'], [' Fighting', ' Bug', ' Fairy'], [' Ghost', ' Dark'], [' Psychic'])
+const steel = new PokeType(' Steel', 'Gray', [' Rock', ' Ice', ' Fairy'], [' Fire', ' Ground', ' Fighting'], [' Normal', ' Grass', ' Ice', ' Flying', ' Psychic', ' Bug', ' Rock', ' Dragon', ' Steel', ' Fairy'], [' Poison'])
+const fairy = new PokeType(' Fairy', 'Pink', [' Fighting', ' Dragon', ' Dark'], [' Poison', ' Dark'], [' Fighting', ' Bug', ' Dark'], [' Dragon'])
 
 //List Container
 const typeName = [opener, normal, fire, water, grass, electric, rock, poison, psychic, ice, ground, flying, fighting, bug, ghost, dragon, dark, steel, fairy]
@@ -110,6 +110,16 @@ function effectivenessCheck () {
             document.querySelector('#test4').hidden = true
         }
 
+        //1x Check Log
+        singleCheck()
+
+        //1x Check
+        function singleCheck(){
+            let singleEffect = defWeakness().concat(doubleResistance()).concat(defResistance()).concat(doubleResistance())
+            singleEffect.filter((item) => item !== Type)
+            console.log(singleEffect)
+        }
+
         //Immunity Check
         function immunityCheck() {
             let defImmune = (sortDef1(input2).immuneTo).concat((sortDef2(input3).immuneTo));
@@ -146,41 +156,41 @@ function effectivenessCheck () {
             //Def1 Type Identifier
             function sortDef1() {
                 let def1;
-                if (input2 === 'Normal') {
+                if (input2 === ' Normal') {
                     def1 = normal
-                } else if (input2 === 'Fire') {
+                } else if (input2 === ' Fire') {
                     def1 = fire
-                } else if (input2 === 'Water') {
+                } else if (input2 === ' Water') {
                     def1 = water
-                } else if (input2 === 'Grass') {
+                } else if (input2 === ' Grass') {
                     def1 = grass
-                } else if (input2 === 'Electric') {
+                } else if (input2 === ' Electric') {
                     def1 = electric
-                } else if (input2 === 'Rock') {
+                } else if (input2 === ' Rock') {
                     def1 = rock
-                } else if (input2 === 'Poison') {
+                } else if (input2 === ' Poison') {
                     def1 = poison
-                } else if (input2 === 'Psychic') {
+                } else if (input2 === ' Psychic') {
                     def1 = psychic
-                } else if (input2 === 'Ice') {
+                } else if (input2 === ' Ice') {
                     def1 = ice
-                } else if (input2 === 'Ground') {
+                } else if (input2 === ' Ground') {
                     def1 = ground
-                } else if (input2 === 'Flying') {
+                } else if (input2 === ' Flying') {
                     def1 = flying
-                } else if (input2 === 'Fighting') {
+                } else if (input2 === ' Fighting') {
                     def1 = fighting
-                } else if (input2 === 'Bug') {
+                } else if (input2 === ' Bug') {
                     def1 = bug
-                } else if (input2 === 'Ghost') {
+                } else if (input2 === ' Ghost') {
                     def1 = ghost
-                } else if (input2 === 'Dragon') {
+                } else if (input2 === ' Dragon') {
                     def1 = dragon
-                } else if (input2 === 'Dark') {
+                } else if (input2 === ' Dark') {
                     def1 = dark
-                } else if (input2 === 'Steel') {
+                } else if (input2 === ' Steel') {
                     def1 = steel
-                } else if (input2 === 'Fairy') {
+                } else if (input2 === ' Fairy') {
                     def1 = fairy
                 }
                 return def1
@@ -189,43 +199,43 @@ function effectivenessCheck () {
             //Def2 Type Identifier
             function sortDef2() {
                 let def2;
-                if (input3 === 'None') {
+                if (input3 === ' None') {
                     def2 = opener
-                } else if (input3 === 'Normal') {
+                } else if (input3 === ' Normal') {
                     def2 = normal
-                } else if (input3 === 'Fire') {
+                } else if (input3 === ' Fire') {
                     def2 = fire
-                } else if (input3 === 'Water') {
+                } else if (input3 === ' Water') {
                     def2 = water
-                } else if (input3 === 'Grass') {
+                } else if (input3 === ' Grass') {
                     def2 = grass
-                } else if (input3 === 'Electric') {
+                } else if (input3 === ' Electric') {
                     def2 = electric
-                } else if (input3 === 'Rock') {
+                } else if (input3 === ' Rock') {
                     def2 = rock
-                } else if (input3 === 'Poison') {
+                } else if (input3 === ' Poison') {
                     def2 = poison
-                } else if (input3 === 'Psychic') {
+                } else if (input3 === ' Psychic') {
                     def2 = psychic
-                } else if (input3 === 'Ice') {
+                } else if (input3 === ' Ice') {
                     def2 = ice
-                } else if (input3 === 'Ground') {
+                } else if (input3 === ' Ground') {
                     def2 = ground
-                } else if (input3 === 'Flying') {
+                } else if (input3 === ' Flying') {
                     def2 = flying
-                } else if (input3 === 'Fighting') {
+                } else if (input3 === ' Fighting') {
                     def2 = fighting
-                } else if (input3 === 'Bug') {
+                } else if (input3 === ' Bug') {
                     def2 = bug
-                } else if (input3 === 'Ghost') {
+                } else if (input3 === ' Ghost') {
                     def2 = ghost
-                } else if (input3 === 'Dragon') {
+                } else if (input3 === ' Dragon') {
                     def2 = dragon
-                } else if (input3 === 'Dark') {
+                } else if (input3 === ' Dark') {
                     def2 = dark
-                } else if (input3 === 'Steel') {
+                } else if (input3 === ' Steel') {
                     def2 = steel
-                } else if (input3 === 'Fairy') {
+                } else if (input3 === ' Fairy') {
                     def2 = fairy
                 }
                 return def2
